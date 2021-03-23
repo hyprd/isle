ed90063153d9bee4158b4802d10fb006c6c1a00e
hyprd <luuzeish@gmail.com>
2021-03-23 13:46:44 +1300

    Send files to desktop

--- il.cpp
+++ il.cpp
@@ -2,7 +2,7 @@
 #include <cstring>
 
 int main(int argc, char *argv[]) {
-    std::string cmdstr = "youtube-dl -i --add-metadata -x --audio-format mp3 --youtube-skip-dash-manifest " + (std::string)argv[1];
+    std::string cmdstr = "youtube-dl -i --add-metadata -x --audio-format mp3 --youtube-skip-dash-manifest --output ~/Desktop/%(title)s.%(ext)s " + (std::string)argv[1];
     system(cmdstr.c_str());
     return 0;
 }
